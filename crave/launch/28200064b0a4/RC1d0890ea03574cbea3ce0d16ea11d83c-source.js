dee5b9e3e1818c7a3940d49559f3e5398466f704
Roman Hosek <roman.hosek@deltatre.com>
2023-07-28 16:29:50 +0200

    TEST

--- crave/launch/28200064b0a4/RC1d0890ea03574cbea3ce0d16ea11d83c-source.js
+++
@@ -1,4 +0,0 @@
-_satellite.__registerScript(
-	'/653e7ca2fc48/16d20b3f6bc7/28200064b0a4/RC1d0890ea03574cbea3ce0d16ea11d83c-source.js',
-	'<!-- Segment Pixel - Homepage - DO NOT MODIFY -->\n<img src="https://secure.adnxs.com/seg?add=14506563&t=2" style="width: 1px; height: 1px; border: 0;" /> \n<!-- End of Segment Pixel -->'
-);
--- crave/launch/28200064b0a4/RC2a2a3c64d1bd439991693121d0b0a563-source.js
+++
@@ -1,4 +0,0 @@
-_satellite.__registerScript(
-	'/653e7ca2fc48/16d20b3f6bc7/28200064b0a4/RC2a2a3c64d1bd439991693121d0b0a563-source.js',
-	"if (document.location.hostname.indexOf('9c9media.com') > -1) {\n\tdocument.dispatchEvent(new CustomEvent('digitalDataLoaded'));\n}\n"
-);
